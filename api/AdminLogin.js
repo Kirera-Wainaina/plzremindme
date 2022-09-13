d44b223d9e84cd4503179fb8a1fbabcaf85a1690
Kirera Wainaina <richardwainainak@gmail.com>
2022-09-13 08:47:19 +0300

    Retrieve user from email

--- api/AdminLogin.js
+++ api/AdminLogin.js
@@ -4,11 +4,19 @@ class AdminLogin extends APIResponder {
     constructor(props) {
         super(props);
         this.data = null;
+        this.user = null;
+    }
+
+    async getUserData() {
+        const collection = await this.getCollection('admins');
+        const user = collection.findOne({ 'email': this.data.email});
+        return user
     }
 
     async run() {
         this.data = await this.retrieveData();
-        console.log(this.data);
+        this.user = await this.getUserData();
+        console.log(this.user)
     }
 }
 
