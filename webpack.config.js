362d1db9fec531bd590c6f4188a9943b7d5cbbd4
Kirera Wainaina <richardwainainak@gmail.com>
2022-09-15 11:03:26 +0300

    Create the config that runs react

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: {
+        // app: './src/App.js',
+        adminApp: path.join(__dirname, 'frontend/src/admin.js')
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'frontend/js'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    }
+}
