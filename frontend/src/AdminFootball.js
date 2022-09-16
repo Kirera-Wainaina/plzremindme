5f3097464237268501ec3bf5ff396266fb07668d
Kirera Wainaina <richardwainainak@gmail.com>
2022-09-16 12:33:33 +0300

    Create an admin football and formula1 page

---
+++ frontend/src/AdminFootball.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AdminFootball() {
+    return <p>She blew the team for some red bottoms</p>
+}
---
+++ frontend/src/AdminFormula1.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AdminFormula1() {
+    return <p>WTF!</p>
+}
