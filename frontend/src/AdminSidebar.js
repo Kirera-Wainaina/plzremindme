b159dcd5e3327dd1a201b103f82af5b37fc74401
Kirera Wainaina <richardwainainak@gmail.com>
2022-09-16 12:27:06 +0300

    Create the side of the admin home page

---
+++ frontend/src/AdminSidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AdminSidebar() {
+    return (
+        <section className="card">
+            <button className="button">Formula 1</button>
+            <button className="button">Football</button>
+        </section>
+    )
+}
