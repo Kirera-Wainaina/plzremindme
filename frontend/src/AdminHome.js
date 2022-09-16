bb8a85cc60c0db81b78241ffbe65da2aa6656c02
Kirera Wainaina <richardwainainak@gmail.com>
2022-09-16 08:22:38 +0300

    Check if logged in before rendering admin/home

--- frontend/src/AdminHome.js
+++ frontend/src/AdminHome.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 export default class AdminHome extends React.Component {
+    confirmLogin() {
+        if (!sessionStorage.getItem('isLoggedIn')) {
+            location.pathname = '/admin/login'
+        }
+    }
+
     render() {
+        this.confirmLogin();
+        
         return (
             <p>Welcome home</p>
         )
