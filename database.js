3da0b41eeb9a9be926f386b3d33a9d58cc0b1a82
Kirera Wainaina <richardwainainak@gmail.com>
2022-09-20 15:28:43 +0300

    Initialize the firestore class
    Input the credentials and export it

--- database.js
+++ database.js
@@ -5,4 +5,12 @@ dotenv.config();
 const uri = `mongodb://${process.env.DB_USERNAME}:\
 ${process.env.DB_PASSWORD}@${process.env.DB_IP}?w=majority`;
 
+const { Firestore } = require('@google-cloud/firestore');
+
+const firestore = new Firestore({
+    projectId: process.env.GCLOUD_PROJECT_ID,
+    keyFilename: process.env.SERVICE_ACCOUNT_PATH
+});
+
 exports.mongoClient = new MongoClient(uri);
+exports.firestore = firestore;
