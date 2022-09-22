c8e5c8ade1f692b859c1686eb74dcf1406158827
Kirera Wainaina <richardwainainak@gmail.com>
2022-09-22 19:33:38 +0300

    Input tabs into the football component

--- frontend/src/AdminFootball.js
+++ frontend/src/AdminFootball.js
@@ -1,10 +1,36 @@
+import { Tab, Tabs } from "@mui/material";
 import React from "react";
 
 export default function AdminFootball() {
+    const [value, setValue] = React.useState(0);
+
+    function handleChange(event, newValue) {
+        setValue(newValue);
+    }
+
     return (
         <div className="admin-component">
+            <Tabs 
+                variant='fullWidth' 
+                onChange={handleChange}
+                value={value}
+            >
+                <Tab label='Upload Matches' value={0}/>
+                <Tab label='Edit Matches' value={1}/>
+            </Tabs>
             <p>She blew the team for some red bottoms</p>
+            <TabPanel value={value} index={0}>Upload Matches</TabPanel>
+            <TabPanel value={value} index={1}>Edit Matches</TabPanel>
 
+        </div>
+    )
+}
+
+function TabPanel(props) {
+    return (
+        <div role='tabpanel' 
+            hidden={props.value != props.index}>
+            {props.children}
         </div>
     )
 }
