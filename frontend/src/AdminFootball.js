a88d2d131d9b7cad47a11b67f774cbbb86696b79
Kirera Wainaina <richardwainainak@gmail.com>
2022-09-22 12:38:52 +0300

    Add ids to admin components

--- frontend/src/AdminFootball.js
+++ frontend/src/AdminFootball.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function AdminFootball() {
-    return <p>She blew the team for some red bottoms</p>
+    return (
+        <div className="admin-component">
+            <p>She blew the team for some red bottoms</p>
+
+        </div>
+    )
 }
--- frontend/src/AdminFormula1.js
+++ frontend/src/AdminFormula1.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function AdminFormula1() {
-    return <p>WTF!</p>
+    return (
+        <div className="category-component">
+            <p>WTF!</p>
+        </div>
+    )
 }
