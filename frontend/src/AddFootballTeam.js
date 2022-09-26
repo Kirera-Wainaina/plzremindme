ff5200afe1802dceb75b3e4124b9db5c57e0b75e
Kirera Wainaina <richardwainainak@gmail.com>
2022-09-26 16:28:24 +0300

    Remove alert when someone starts typing again

--- frontend/src/AddFootballTeam.js
+++ frontend/src/AddFootballTeam.js
@@ -23,7 +23,7 @@ export default class AddFootballTeam extends React.Component {
     }
 
     handleChange(e) {
-        this.setState({ [e.target.name]: e.target.value })
+        this.setState({ [e.target.name]: e.target.value, statusCode: null })
     }
 
     openFileUpload(e) {
