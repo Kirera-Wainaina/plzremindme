1603615a4207f231e88830c98e42a3d41118b621
Kirera Wainaina <richardwainainak@gmail.com>
2022-09-27 11:47:28 +0300

    Add grids to the search panel

--- frontend/src/EditFootballTeam.js
+++ frontend/src/EditFootballTeam.js
@@ -7,20 +7,28 @@ export default class EditFootballTeam extends React.Component {
         return (
             <Grid container direction='column' spacing={2}>
 
-                <Grid item>
+                <Grid item xs={12}>
                     <Card sx={{
                         width: '100%',
                         height: '10%',
                         mt: '3%',
                         paddingBottom: '5px'
                     }}>
-                        <IconButton size='large'><FilterList color="primary"/></IconButton>
-                        <TextField variant="filled" placeholder="Enter team name..." sx={{width: '87%'}}/>
-                        <IconButton size='large'><Search variant='filled' color="primary"/></IconButton>
+                        <Grid container direction='row'>
+                            <Grid item xs={2} sm={1}>
+                                <IconButton size='large'><FilterList color="primary"/></IconButton>
+                            </Grid>
+                            <Grid item xs={8} sm={10}>
+                                <TextField variant="filled" placeholder="Enter team name..." sx={{ width: '100%'}}/>
+                            </Grid>
+                            <Grid item xs={2} sm={1}>
+                                <IconButton size='large'><Search variant='filled' color="primary"/></IconButton>
+                            </Grid>
+                        </Grid>
                     </Card>
                 </Grid>
 
-                <Grid item>
+                <Grid item xs={12}>
                     <Card sx={{ 
                         width: '100%',
                         height: '50%'
