ec04fbdc67aaf3dc657530dbc5cf4cc956aedfbb
Kirera Wainaina <richardwainainak@gmail.com>
2022-10-01 12:20:17 +0300

    Create the EditFootballTeam class

---
+++ api/admin/EditFootballTeam.js
@@ -0,0 +1,43 @@
+const { FieldPath } = require("@google-cloud/firestore");
+const { FormDataHandler } = require ("../..");
+const CloudFileEditor = require("../../handlers/CloudFileEditor");
+
+
+class EditFootballTeam extends FormDataHandler {
+    constructor(props) {
+        super(props);
+    }
+
+    createUpdateObject() {
+        this.fields['logoName'] = this.logoMetadata.name;
+        this.fields['logoLink'] = this.logoMetadata.mediaLink;
+        this.fields['logoId'] = this.logoMetadata.id;
+    }
+
+    async run(response) {
+        await this.retrieveData();
+
+        if (this.uploadedFilePath) { // there is a new file
+            const editor = new CloudFileEditor(this.uploadedFilePath, this.fields.logoName);
+            this.logoMetadata = await editor.run();
+            this.createUpdateObject()
+        }
+
+        const updateResult = await this.getCollection('football-teams')
+            .where(FieldPath.documentId(), '==', this.fields.docId)
+            .get().then(querySnapshot => {
+                const ref = querySnapshot.docs[0].ref;
+                delete this.fields.docId;
+                return ref.update(this.fields)
+            })
+
+        if (updateResult.writeTime) {
+            console.log('Changes made successfully')
+            this.respond(response, 'success');
+        }
+
+        // console.log(this.fields);
+    }
+}
+
+module.exports = EditFootballTeam;
