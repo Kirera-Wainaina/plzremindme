1c03c7afb101c0f28cf77a9ae0dedf15352aa079
Kirera Wainaina <richardwainainak@gmail.com>
2022-10-02 10:26:24 +0300

    Create a Filter Component

--- frontend/src/EditFootballTeam.js
+++ frontend/src/EditFootballTeam.js
@@ -12,19 +12,7 @@ export default class EditFootballTeam extends React.Component {
             <Grid container justifyContent='center' spacing={2}>
 
                 <Grid item xs={12} sm={8} sx={{ mt: '1%'}}>
-                    <Card>
-                        <Grid container direction='row'>
-                            <Grid item xs={2} sm={1}>
-                                <IconButton size='large'><FilterList color="primary"/></IconButton>
-                            </Grid>
-                            <Grid item xs={8} sm={10}>
-                                <TextField variant="filled" placeholder="Enter team name..." sx={{ width: '100%'}}/>
-                            </Grid>
-                            <Grid item xs={2} sm={1}>
-                                <IconButton size='large'><Search variant='filled' color="primary"/></IconButton>
-                            </Grid>
-                        </Grid>
-                    </Card>
+                    <Filter />
                 </Grid>
 
                 <Grid item xs={12} sm={8}>
@@ -32,6 +20,27 @@ export default class EditFootballTeam extends React.Component {
                 </Grid>
 
             </Grid>
+        )
+    }
+}
+
+class Filter extends React.Component {
+    render() {
+        return (
+            <Card>
+                <Grid container direction='row'>
+                    <Grid item xs={2} sm={1}>
+                        <IconButton size='large'><FilterList color="primary"/></IconButton>
+                    </Grid>
+                    <Grid item xs={8} sm={10}>
+                        <TextField variant="filled" placeholder="Enter team name..." sx={{ width: '100%'}}/>
+                    </Grid>
+                    <Grid item xs={12} sm={12}>
+                        <p>Filtered Items</p>
+                    </Grid>
+                </Grid>
+            </Card>
+
         )
     }
 }
