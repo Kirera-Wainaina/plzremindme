bf8f093085defea21531d8486f9e874c1b84a29b
Kirera Wainaina <richardwainainak@gmail.com>
2022-10-07 09:46:51 +0300

    Respond with leagues and tournaments

---
+++ api/admin/GetLeaguesAndTournaments.js
@@ -0,0 +1,20 @@
+const { GETHandler } = require("../..");
+
+class GetLeaguesAndTournaments extends GETHandler {
+    constructor(props) {
+        super(props);
+
+        this.data = [];
+    }
+
+    async run() {
+        const snapshot = await this.getCollection('leagues-tournaments').get();
+        snapshot.forEach(doc => {
+            this.data.push({...doc.data(), docId: doc.id})
+        });
+        this.respond('success', JSON.stringify(this.data));
+    }
+
+}
+
+module.exports = GetLeaguesAndTournaments;
