3309f0f4c15a6f709fc2887162633ca8fe6997ec
Kirera Wainaina <richardwainainak@gmail.com>
2022-10-07 09:46:23 +0300

    Get the leagues and tournaments

--- frontend/src/EditLeaguesAndTournaments.js
+++ frontend/src/EditLeaguesAndTournaments.js
@@ -1,9 +1,49 @@
+import { Grid } from "@mui/material";
 import React from "react";
 
 export default class EditLeaguesAndTournaments extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            categories: []
+        }
+    }
+
     render() {
         return (
-            <p>Hello</p>
+            <Grid container justifyContent='center' spacing={2}>
+
+                <Grid item xs={12} sm={8} sx={{ mt: '1%'}}>
+                    <Filter 
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={8}>
+                    <LeaguesAndTournaments
+                        categories={this.state.categories}
+                        setCategories={categories => this.setState({ categories })}
+                    />
+                </Grid>
+
+            </Grid>
         )
     }
+}
+
+class Filter extends React.Component {
+    render() {
+
+    }
+}
+
+function LeaguesAndTournaments(props) {
+    const [contentId, setContentId] = React.useState(null);
+    const [modalIsOpen, setModalIsOpen] = React.useState(false);
+
+    React.useEffect(() => {
+        fetch('/api/admin/GetLeaguesAndTournaments')
+            .then(response => response.json())
+            .then(data => props.setCategories(data))
+    }, [])
 }
