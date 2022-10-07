8109ed342a83aac269137b282786ca048b25a596
Kirera Wainaina <richardwainainak@gmail.com>
2022-10-07 10:51:44 +0300

    Handle error that could occur while uploading football team

--- api/admin/EditFootballTeam.js
+++ api/admin/EditFootballTeam.js
@@ -15,28 +15,31 @@ class EditFootballTeam extends FormDataHandler {
     }
 
     async run(response) {
-        await this.retrieveData();
+        try {
+            await this.retrieveData();
 
-        if (this.uploadedFilePath) { // there is a new file
-            const editor = new CloudFileEditor(this.uploadedFilePath, this.fields.logoName);
-            this.logoMetadata = await editor.run();
-            this.createUpdateObject()
+            if (this.uploadedFilePath) { // there is a new file
+                const editor = new CloudFileEditor(this.uploadedFilePath, this.fields.logoName);
+                this.logoMetadata = await editor.run();
+                this.createUpdateObject()
+            }
+
+            const updateResult = await this.getCollection('football-teams')
+                .where(FieldPath.documentId(), '==', this.fields.docId)
+                .get().then(querySnapshot => {
+                    const ref = querySnapshot.docs[0].ref;
+                    delete this.fields.docId;
+                    return ref.update(this.fields)
+                })
+
+            if (updateResult.writeTime) {
+                console.log('Changes made successfully')
+                this.respond(response, 'success');
+            }
+        } catch (error) {
+            console.log('Error while editing football team: ', error);
+            this.respond(response, 'error');
         }
-
-        const updateResult = await this.getCollection('football-teams')
-            .where(FieldPath.documentId(), '==', this.fields.docId)
-            .get().then(querySnapshot => {
-                const ref = querySnapshot.docs[0].ref;
-                delete this.fields.docId;
-                return ref.update(this.fields)
-            })
-
-        if (updateResult.writeTime) {
-            console.log('Changes made successfully')
-            this.respond(response, 'success');
-        }
-
-        // console.log(this.fields);
     }
 }
 
