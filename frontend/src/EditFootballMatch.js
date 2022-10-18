2f9580bbff9b24140b6a816497663ec04fc369f4
Kirera Wainaina <richardwainainak@gmail.com>
2022-10-18 16:46:22 +0300

    Create the textfield in the filter tab
    Entering team name will return all it's matches

--- frontend/src/EditFootballMatch.js
+++ frontend/src/EditFootballMatch.js
@@ -1,5 +1,5 @@
 import { FilterList } from "@mui/icons-material";
-import { Card, Grid, IconButton } from "@mui/material";
+import { Card, Grid, IconButton, TextField } from "@mui/material";
 import React from "react";
 
 export default class EditFootballMatch extends React.Component {
@@ -11,7 +11,7 @@ export default class EditFootballMatch extends React.Component {
         return (
             <Grid container justifyContent='center' spacing={2}>
 
-                <Grid item xs={12} sm={6} sx={{ mt: '1%' }}>
+                <Grid item xs={12} sm={8} sx={{ mt: '1%' }}>
                     <Filter />
                 </Grid>
             </Grid>
@@ -26,6 +26,12 @@ class Filter extends React.Component {
         this.state = {
             showFilterModal: false
         }
+
+        this.searchMatchThroughInput = this.searchMatchThroughInput.bind(this);
+    }
+
+    searchMatchThroughInput(e) {
+
     }
 
     render() {
@@ -44,6 +50,17 @@ class Filter extends React.Component {
                             <FilterList color="primary"/>
                         </IconButton>
                     </Grid>
+
+                    <Grid item xs={8} sm={10}>
+                        <TextField variant="filled" 
+                            placeholder="Enter team name to get it's matches..." 
+                            size="small"
+                            margin="normal"
+                            onChange={this.searchMatchThroughInput}
+                            fullWidth
+                            />
+                    </Grid>
+
                 </Grid>
             </Card>
         )
