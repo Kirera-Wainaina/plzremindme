884e573a7613adb3907b8ebba8adc4ee472d700d
Kirera Wainaina <richardwainainak@gmail.com>
2022-10-21 19:02:17 +0300

    Display the football menu at top for sm viewport
    The drop down has a position: 'fixed' to prevent it from constantly
    pushing other content up and down.

--- frontend/src/AdminFootball.js
+++ frontend/src/AdminFootball.js
@@ -1,4 +1,5 @@
-import { Divider, Grid, Tab, Tabs } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Divider, Grid, 
+    Tab, Tabs, Typography, useMediaQuery, useTheme } from "@mui/material";
 import React from "react";
 import AddFootballTeam from "./AddFootballTeam";
 import EditFootballTeam from "./EditFootballTeam";
@@ -6,8 +7,11 @@ import AddFootballCompetition from "./AddFootballCompetition";
 import EditFootballCompetition from "./EditFootballCompetition";
 import AddFootballMatch from "./AddFootballMatch";
 import EditFootballMatch from "./EditFootballMatch";
+import { ExpandMore } from "@mui/icons-material";
 
 export default function AdminFootball() {
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up('sm'))
     const [value, setValue] = React.useState(0);
 
     function handleChange(event, newValue) {
@@ -16,70 +20,105 @@ export default function AdminFootball() {
 
     return (
         <Grid container>
-            <Grid item xs={12} sm={2}>
-                <Tabs 
-                    onChange={handleChange}
-                    value={value}
-                    orientation='vertical'
-                    sx={{
-                        borderRight: 1,
-                        borderColor: 'divider',
-                        height: {
-                            sm: '100vh'
-                        }
-                    }}
-                >
-                    <Tab label='Add Teams' value={0} 
-                        sx={{ 
-                            display: 'flex', 
-                            alignItems: 'flex-start',
-                            ml: '5%'
-                        }}
-                    />
-                    <Divider />
-                    <Tab label='Edit Teams' value={1}
-                        sx={{ 
-                            display: 'flex', 
-                            alignItems: 'flex-start',
-                            ml: '5%'
-                        }}
-                    />
-                    <Divider />
-                    <Tab label='Add Competitions' value={2}
-                        sx={{ 
-                            display: 'flex', 
-                            alignItems: 'flex-start',
-                            ml: '5%'
-                        }}
-                    />
-                    <Divider />
-                    <Tab label='Edit Competitions' value={3}
-                        sx={{ 
-                            display: 'flex', 
-                            alignItems: 'flex-start',
-                            ml: '5%'
-                        }}
-                    />
-                    <Divider />
-                    <Tab label='Add Matches' value={4}
-                        sx={{ 
-                            display: 'flex', 
-                            alignItems: 'flex-start',
-                            ml: '5%'
-                        }}
-                    />
-                    <Divider />
-                    <Tab label='Edit Matches' value={5}
-                        sx={{ 
-                            display: 'flex', 
-                            alignItems: 'flex-start',
-                            ml: '5%'
-                        }}
-                    />
-                    <Divider />
-                </Tabs>
 
+            <Grid item xs={12} sm={2}>
+                <Accordion 
+                    defaultExpanded={matches}
+                    sx={{ 
+                        // position: {
+                            // xs: 'fixed',
+                            // sm: 'static'
+                        // },
+                        // width: {
+                            // xs: '100vw',
+                            // sm: '100%'
+                        // },
+                        // zIndex: 1,
+                        // backgroundColor: 'white'
+                     }}
+                >
+                    <AccordionSummary
+                        expandIcon={<ExpandMore />}
+                    >
+                        <Typography variant='body1'>Football Menu</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails
+                        sx={{
+                            position: {
+                                xs: "fixed",
+                                sm: 'static'
+                            },
+                            width: {
+                                xs: '100vw',
+                                sm: 'inherit'
+                            },
+                            zIndex: 2,
+                            backgroundColor: 'white'
+                        }}
+                    >
+                        <Tabs 
+                            onChange={handleChange}
+                            value={value}
+                            orientation='vertical'
+                            sx={{
+                                height: {
+                                    sm: '100vh'
+                                }
+                            }}
+                        >
+                            <Tab label='Add Teams' value={0} 
+                                sx={{ 
+                                    display: 'flex', 
+                                    alignItems: 'flex-start',
+                                    ml: '5%'
+                                }}
+                            />
+                            <Divider />
+                            <Tab label='Edit Teams' value={1}
+                                sx={{ 
+                                    display: 'flex', 
+                                    alignItems: 'flex-start',
+                                    ml: '5%'
+                                }}
+                            />
+                            <Divider />
+                            <Tab label='Add Competitions' value={2}
+                                sx={{ 
+                                    display: 'flex', 
+                                    alignItems: 'flex-start',
+                                    ml: '5%'
+                                }}
+                            />
+                            <Divider />
+                            <Tab label='Edit Competitions' value={3}
+                                sx={{ 
+                                    display: 'flex', 
+                                    alignItems: 'flex-start',
+                                    ml: '5%'
+                                }}
+                            />
+                            <Divider />
+                            <Tab label='Add Matches' value={4}
+                                sx={{ 
+                                    display: 'flex', 
+                                    alignItems: 'flex-start',
+                                    ml: '5%'
+                                }}
+                            />
+                            <Divider />
+                            <Tab label='Edit Matches' value={5}
+                                sx={{ 
+                                    display: 'flex', 
+                                    alignItems: 'flex-start',
+                                    ml: '5%'
+                                }}
+                            />
+                            <Divider />
+                        </Tabs>
+                    </AccordionDetails>
+                </Accordion>
             </Grid>
+
             <Grid item xs={12} sm={10}>
                 <TabPanel value={value} index={0}>
                     <AddFootballTeam />
